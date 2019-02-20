94c415f79740c432e18e6cc21463ec359b40406c
Joana <joanajteixeira@outlook.com>
2019-02-20 20:18:14 +0000

    //Basic Data Structures: Create complex multi-dimensional arrays
    /*We have defined a variable, myNestedArray, set equal to an array. Modify myNestedArray, using any combination of strings, numbers, and booleans for data elements, so that it has exactly five levels of depth (remember, the outer-most array is level 1). Somewhere on the third level, include the string 'deep', on the fourth level, include the string 'deeper', and on the fifth level, include the string 'deepest'.*/

---
+++ generateArrayOfAllObjectKeysWithObjectkeys.js
@@ -0,0 +1,30 @@
+//Basic Data Structures: Generate an Array of All Object Keys with Object.keys()
+
+//Finish writing the getArrayOfUsers function so that it returns an array containing all the properties in the object it receives as an argument.
+
+let users = {
+    Alan: {
+      age: 27,
+      online: false
+    },
+    Jeff: {
+      age: 32,
+      online: true
+    },
+    Sarah: {
+      age: 48,
+      online: false
+    },
+    Ryan: {
+      age: 19,
+      online: true
+    }
+  };
+  
+  function getArrayOfUsers(obj) {
+    // change code below this line
+      return Object.keys(users);
+    // change code above this line
+  }
+  
+  console.log(getArrayOfUsers(users));
