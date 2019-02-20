17cb8cad9ea670b0fcba6320a1e5510b97a5cb08
Joana <joanajteixeira@outlook.com>
2019-02-20 20:19:43 +0000

    //Basic Data Structures: Remove Items Using splice()
    //We've defined a function, sumOfTen, which takes an array as an argument and returns the sum of that array's elements. Modify the function, using splice(), so that it returns a value of 10.

---
+++ removeItemsUsing-splice.js
@@ -0,0 +1,13 @@
+//Basic Data Structures: Remove Items Using splice()
+
+//We've defined a function, sumOfTen, which takes an array as an argument and returns the sum of that array's elements. Modify the function, using splice(), so that it returns a value of 10.
+
+function sumOfTen(arr) {
+    // change code below this line
+      arr.splice(2, 2);
+    // change code above this line
+    return arr.reduce((a, b) => a + b);
+  }
+  
+  // do not change code below this line
+  console.log(sumOfTen([2, 5, 1, 5, 2, 1]));
