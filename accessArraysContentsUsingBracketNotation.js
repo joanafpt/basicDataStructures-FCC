d4b40603d02851894992fef73201b5c98cba97b9
Joana <joanajteixeira@outlook.com>
2019-02-20 20:16:18 +0000

    //Basic Data Structures: Access an Array's Contents Using Bracket Notation
    //In order to complete this challenge, set the 2nd position (index 1) of myArray to anything you want, besides "b".

---
+++ accessArraysContentsUsingBracketNotation.js
@@ -0,0 +1,9 @@
+//Basic Data Structures: Access an Array's Contents Using Bracket Notation
+
+//In order to complete this challenge, set the 2nd position (index 1) of myArray to anything you want, besides "b".
+
+let myArray = ["a", "b", "c", "d"];
+// change code below this line
+myArray[1] = "boooh";
+//change code above this line
+console.log(myArray); //logs a,boooh,c,d
